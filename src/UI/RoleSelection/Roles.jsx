1ba735ba4db24d593ccd2c0ab5387099c3935853
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-04-16 15:46:10 -0700

    arreglé la sección de selección de roles y la hice responsive

--- src/UI/RoleSelection/Roles.jsx
+++ src/UI/RoleSelection/Roles.jsx
@@ -1,36 +1,37 @@
-import React from 'react';
-import './roles.css';
-import estudianteIcon from './estudiante.png';
-import profesorIcon from './profesor.jpg';
-import adminIcon from './adm.png';
-import Navbar from '../RoleSelection/Navbar/navbarRoles';
+import React from "react";
+import "./roles.css";
+import imagenEstudiante from "./estudiante.png";
+import imagenProfesor from "./profesor.jpg";
 
 const Roles = () => {
   return (
-
     <div className="roles-container">
-      <div className="navbar-wrapper">
-        <Navbar />
-      </div>
-      
       <main className="main-content">
-        <div className="roles">
-          <div className="role-card">
-            <img src={estudianteIcon} alt="Estudiante" className="role-icon" />
-            <h2 className="role-title">Un estudiante</h2>
-            <p className="role-description">para participar en actividades divertidas en el aula</p>
-          </div>
-          
-          <div className="role-card">
-            <img src={profesorIcon} alt="Profesor" className="role-icon" />
-            <h2 className="role-title">Un profesor</h2>
-            <p className="role-description">instruir, involucrar y evaluar a mis alumnos</p>
-          </div>
-          
-          <div className="role-card">
-            <img src={adminIcon} alt="Administrador" className="role-icon" />
-            <h2 className="role-title">Un administrador</h2>
-            <p className="role-description">entrenador de instrucción, currículo o líder escolar</p>
+        <div className="inner-content">
+          <h3 className="text-center verde">
+            <strong>ActiveClassroom</strong>
+          </h3>
+          <p className="text-center verde">Por favor, selecciona tu rol</p>
+          <div className="roles">
+            <div className="role-card">
+              <img
+                src={imagenEstudiante}
+                alt="Estudiante"
+                className="role-icon"
+              />
+              <h2 className="role-title">Un estudiante</h2>
+              <p className="role-description">
+                Para participar en actividades divertidas en el aula.
+              </p>
+            </div>
+
+            <div className="role-card">
+              <img src={imagenProfesor} alt="Profesor" className="role-icon" />
+              <h2 className="role-title">Un profesor</h2>
+              <p className="role-description">
+                Instruir, involucrar y evaluar a mis alumnos.
+              </p>
+            </div>
           </div>
         </div>
       </main>
